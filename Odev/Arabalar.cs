cbe858b87cb0300ab68c2fb0ebe7ac8f6a33df96
erdem <erdem@WHITEROSE>
2025-02-26 11:29:35 +0300

    Proje dosyası ekle.

---
+++ Odev/Arabalar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev
+{
+     class Arabalar
+    {
+
+       public string marka;
+       public   string vites;
+    public  void Ozellikler()
+        {
+
+            Console.WriteLine(marka + " marka araba" + " " + vites + "dir");
+        }
+    }
+}
---
+++ Odev/Program.cs
@@ -0,0 +1,33 @@
+﻿using Odev;
+
+public class Mesaj
+{
+    static void Main()
+    {
+        Bmw araba1 = new Bmw();
+        araba1.marka = ("BMW");
+        araba1.vites = ("Otamatik");
+        araba1.Ozellikler();
+       
+        
+        Porshce araba2 = new Porshce();
+        araba2.marka = ("Porsche");
+        araba2.vites = ("Manuel");
+        araba2.Ozellikler();
+
+        Audi araba3 = new Audi();
+        araba3.marka = ("Audi");
+        araba3.vites = ("Yarı otamatik");
+        araba3.Ozellikler();
+
+        Toyota araba4 = new Toyota();
+        araba4.marka=("Toyota");
+        araba4.vites = ("Otomatik");
+        araba4.Ozellikler();
+        
+    
+    
+    
+    
+    }
+}
